acb97862ff6516ac8160a84dfeb7ee3dcbb8b051
answerLion <wlglion@163.com>
2017-09-21 22:53:53 +0800

    完成启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -10,8 +10,37 @@ define([
         success: function (data) {
             // console.log(data.result)
             //解析数据渲染页面
-            var html = template('teacherTpl', {list:data.result})
+            var html = template('teacherTpl', { list: data.result })
             $('#teacherInfo').html(html)
+
+            //启用注销功能
+            $('.eod').on('click', function () {
+                var that = this
+                var td = $(this).closest('td')
+                var tcId = td.attr('data-tcId')
+                var status = td.attr('data-status')
+                console.log(status)
+                $.ajax({
+                    type: 'post',
+                    url: '/api/teacher/handle',
+                    data: {
+                        tc_id: tcId,
+                        tc_status: status
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                        console.log(data.result.tc_status)
+                        if (data.code == 200) {
+                            td.attr('data-status', data.result.tc_status)
+                            if (data.result.tc_status == 0) {
+                                $(that).text('注销')
+                            } else {
+                                $(that).text('启用')
+                            }
+                        }
+                    }
+                })
+            })
         }
     })
 
