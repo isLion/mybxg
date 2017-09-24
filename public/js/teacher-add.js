15c586bbb0cc1dabde012bae3325c9a25a57cad7
answerLion <wlglion@163.com>
2017-09-24 22:56:25 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -17,11 +17,36 @@ define([
                 data.result.operate = '编辑讲师'
                 var html = template('teacherTpl', data.result)
                 $('#teacherInfo').html(html)
+                //处理表单提交
+                submitForm('/api/teacher/update')
             }
         })
     } else {
+        //添加讲师
         var html = template('teacherTpl', { operate: '添加讲师' })
         $('#teacherInfo').html(html)
+        //处理表单提交
+        submitForm('/api/teacher/add')
     }
+
+
+    //封装  ！！！ 实现表单提交
+    function submitForm(url) {
+        $('#teacherBtn').click(function () {
+            $.ajax({
+                type: 'post',
+                url: url,
+                data: $('#teacherForm').serialize(),
+                dataType: 'json',
+                success: function (data) {
+                    if (data.code == 200) {
+                        location.href = '/teacher/list'
+                    }
+                }
+            })
+        })
+    }
+
+
 });
 
